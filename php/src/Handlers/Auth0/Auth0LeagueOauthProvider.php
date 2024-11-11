f5dca01beb529a0f27149df1edf8ad46eee3c002
TJ Draper <tdraper@rxante.com>
2024-11-11 14:47:20 -0600

    Add Auth0LeagueOauthProvider

---
+++ php/src/Handlers/Auth0/Auth0LeagueOauthProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RxAnte\OAuth\Handlers\Auth0;
+
+use League\OAuth2\Client\Provider\GenericProvider;
+
+class Auth0LeagueOauthProvider extends GenericProvider
+{
+    protected string $audience;
+
+    /** @return string[] */
+    protected function getConfigurableOptions(): array
+    {
+        $options = parent::getConfigurableOptions();
+
+        $options[] = 'audience';
+
+        return $options;
+    }
+
+    /** @return string[] */
+    protected function getRequiredOptions(): array
+    {
+        $options = parent::getRequiredOptions();
+
+        $options[] = 'audience';
+
+        return $options;
+    }
+
+    /**
+     * @param mixed[] $options
+     *
+     * @return string[]
+     */
+    protected function getAuthorizationParameters(array $options): array
+    {
+        $params = parent::getAuthorizationParameters($options);
+
+        $params['audience'] = $this->audience;
+
+        return $params;
+    }
+}
