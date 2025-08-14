0ada852e761f3375be3ed4df49c987a212e05d64
TJ Draper <tdraper@rxante.com>
2025-08-14 16:12:08 -0500

    Set up Javascript syncing

--- javascript/copy-to-rxeffect.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ../../rxeffect/web/node_modules/rxante-oauth
-rm ../../rxeffect/web/node_modules/rxante-oauth/package.json
-rm -rf ../../rxeffect/web/node_modules/rxante-oauth/*
-cp package.json ../../rxeffect/web/node_modules/rxante-oauth/package.json
-cp pnpm-lock.yaml ../../rxeffect/web/node_modules/rxante-oauth/pnpm-lock.yaml
-cp -r dist ../../rxeffect/web/node_modules/rxante-oauth/dist
-cp -r src ../../rxeffect/web/node_modules/rxante-oauth/src
---
+++ js-build-and-sync.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+// An array of paths to copy the `javascript` directory to any time a typescript build has completed
+return [
+    '/Users/tdraper209/repos/rxante/rxante_auth/local-next-test-client/node_modules/rxante-oauth',
+];
