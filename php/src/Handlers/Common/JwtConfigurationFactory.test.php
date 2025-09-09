5eb26192eebf00252a2885034efbe615eea14a3e
TJ Draper <tdraper@rxante.com>
2025-09-09 16:25:33 -0500

    Add ProviderOptionsReader test and JwtConfigurationFactory test

---
+++ php/src/Handlers/Common/JwtConfigurationFactory.test.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RxAnte\OAuth\Tests\Handlers\Common;
+
+use Lcobucci\JWT\Configuration as JwtConfiguration;
+use Mockery;
+use Mockery\MockInterface;
+use Psr\Clock\ClockInterface;
+use RuntimeException;
+use RxAnte\OAuth\Handlers\Common\JwtConfigurationFactory;
+use RxAnte\OAuth\Handlers\Common\OauthPublicKey;
+
+use function describe;
+use function expect;
+use function it;
+use function uses;
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+// phpcs:disable SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingTraversableTypeHintSpecification
+
+describe('JwtConfigurationFactory', function (): void {
+    uses()->group('JwtConfigurationFactory');
+
+    readonly class JwtConfigurationFactoryTestSetup
+    {
+        public JwtConfigurationFactory $sut;
+        public MockInterface&ClockInterface $clock;
+        public MockInterface&OauthPublicKey $publicKey;
+
+        public function __construct()
+        {
+            $this->clock = Mockery::mock(ClockInterface::class);
+
+            $this->publicKey = Mockery::mock(OauthPublicKey::class);
+
+            $this->sut = new JwtConfigurationFactory(
+                clock: $this->clock,
+                publicKey: $this->publicKey,
+            );
+        }
+    }
+
+    it(
+        'throws an exception if the public key is empty',
+        function (): void {
+            $setup = new JwtConfigurationFactoryTestSetup();
+
+            $setup->publicKey->expects('getKeyContents')
+                ->andReturn('');
+
+            $setup->sut->create();
+        },
+    )->throws(
+        RuntimeException::class,
+        'Public key is empty',
+    );
+
+    it(
+        'creates a JWT configuration when the public key is valid',
+        function (
+            string|null $passPhrase,
+        ): void {
+            $setup = new JwtConfigurationFactoryTestSetup();
+
+            $setup->publicKey->expects('getKeyContents')
+                ->andReturn('mock-key-contents');
+
+            $setup->publicKey->expects('getPassPhrase')
+                ->andReturn($passPhrase);
+
+            $result = $setup->sut->create();
+
+            expect($result)->toBeInstanceOf(
+                JwtConfiguration::class,
+            );
+        },
+        /** @phpstan-ignore-next-line */
+    )->with([
+        'with passphrase' => 'mock-passphrase',
+        'without passphrase' => null,
+    ]);
+});
---
+++ php/src/Handlers/Common/ProviderOptionsReader.test.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RxAnte\OAuth\Tests\Handlers\Common;
+
+use League\OAuth2\Client\Provider\GenericProvider;
+use RxAnte\OAuth\Handlers\Common\ProviderOptionsReader;
+
+use function describe;
+use function expect;
+use function it;
+use function uses;
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+// phpcs:disable Squiz.Classes.ClassFileName.NoMatch
+// phpcs:disable SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingTraversableTypeHintSpecification
+
+describe('ProviderOptionsReader', function (): void {
+    uses()->group('ProviderOptionsReader');
+
+    readonly class ProviderOptionsReaderTestSetup
+    {
+        public ProviderOptionsReader $sut;
+
+        public function __construct(
+            string|null $clientId,
+            string|int|null $clientSecret,
+        ) {
+            $this->sut = new ProviderOptionsReader(
+                new GenericProvider([
+                    'clientId' => $clientId,
+                    'clientSecret' => $clientSecret,
+                    'urlAuthorize' => 'https://auth',
+                    'urlAccessToken' => 'https://token',
+                    'urlResourceOwnerDetails' => 'https://deets',
+                ]),
+            );
+        }
+    }
+
+    it('returns the client id', function (): void {
+        $setup = new ProviderOptionsReaderTestSetup(
+            clientId: 'mock-client-id',
+            clientSecret: 'mock-client-secret',
+        );
+
+        $result = $setup->sut->clientId();
+
+        expect($result)->toBe('mock-client-id');
+    });
+
+    it('returns the client secret', function (): void {
+        $setup = new ProviderOptionsReaderTestSetup(
+            clientId: 'mock-client-id',
+            clientSecret: 'mock-client-secret',
+        );
+
+        $result = $setup->sut->clientSecret();
+
+        expect($result)->toBe('mock-client-secret');
+    });
+
+    it(
+        'returns an empty string if client id is not set',
+        function (): void {
+            $setup = new ProviderOptionsReaderTestSetup(
+                clientId: null,
+                clientSecret: 'mock-client-secret',
+            );
+
+            $result = $setup->sut->clientId();
+
+            expect($result)->toBe('');
+        },
+    );
+
+    it(
+        'returns an empty string if client secret is not set',
+        function (): void {
+            $setup = new ProviderOptionsReaderTestSetup(
+                clientId: 'mock-client-id',
+                clientSecret: 123,
+            );
+
+            $result = $setup->sut->clientSecret();
+
+            expect($result)->toBe('');
+        },
+    );
+});
